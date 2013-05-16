f8865d791efe5f5de53f40d9be91484ca56ad04d
David Nielsen <david@graveyard.dk>
2013-05-16 19:20:38 +0200

    minor cleanup in bindings

--- config/bindings.el
+++ config/bindings.el
@@ -4,10 +4,6 @@
 (define-key global-map (kbd "C-+") 'text-scale-increase)
 (define-key global-map (kbd "C--") 'text-scale-decrease)
 
-(define-key paredit-mode-map [(control shift right)]    'live-paredit-next-top-level-form)
 
-
-(global-set-key [(control right)] 'forward-word)
-(global-set-key [(control left)] 'backward-word)
-(define-key paredit-mode-map  [ (control shift right )]   'live-paredit-forward)
-; (tester (ytester))
+(global-set-key [(control right)] 'forward-sentence)
+(global-set-key [(control left)] 'backward-sentence)
